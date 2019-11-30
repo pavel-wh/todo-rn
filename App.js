a8b67449d8c1731d78fb73ecab2a679a10f4ef9c
pB <pavel.wh@yandex.ru>
2019-11-30 13:23:02 +0300

    Init project

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Руддщ up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f3f3f3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
