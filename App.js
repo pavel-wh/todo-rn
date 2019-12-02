f8c1e3244e12bbcceee3effb4ab1c5e44aef0427
pB <pavel.wh@yandex.ru>
2019-12-02 16:28:40 +0300

    Работа с картинкам

--- App.js
+++ App.js
@@ -5,16 +5,16 @@ import { MainScreen } from './src/screens/MainScreen'
 import { ToDoScreen } from './src/screens/ToDoScreen'
 
 export default function App() {
-  const [todoId, setTodoId] = useState('2')
+  const [todoId, setTodoId] = useState(null)
   const [todos, setTodos] = useState([
     { 
       id: '1',
       title: 'Выучить React Native'
     },
-    { 
-      id: '2',
-      title: 'Написать приложение'
-    }
+    // { 
+    //   id: '2',
+    //   title: 'Написать приложение'
+    // }
   ])
 
   const addTodo = (title) => {
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,44 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Text, Image } from 'react-native'
 import { AddTodo } from '../components/AddTodo'
 import { ToDo } from '../components/ToDo'
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+
+    let content = (
+        <FlatList 
+            keyExtractor={ item => item.id.toString() }
+            data={ todos }
+            renderItem={ ({item}) => (
+                <ToDo todo={ item } onRemove={ removeTodo } onOpen={ openTodo } />
+            )}
+        />
+    )
+    
+    if (todos.length === 0) {
+        content = <View style={ styles.imageWrapper }>
+            <Image style={ styles.image } source={ require('../../assets/no-items.png') } />
+        </View>
+    }
+
     return (
         <View>
             <AddTodo onSubmit={ addTodo } />
-            <FlatList 
-                keyExtractor={ item => item.id.toString() }
-                data={ todos }
-                renderItem={ ({item}) => (
-                    <ToDo todo={ item } onRemove={ removeTodo } onOpen={ openTodo } />
-                )}
-            />
+            { content }
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    imageWrapper: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        height: 300
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
 })
