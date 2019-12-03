2dfac61d2743e18081ed51376554f7e274145a23
pB <pavel.wh@yandex.ru>
2019-12-03 12:24:58 +0300

    Определение платформы

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from  'react-native'
+import { View, StyleSheet, Platform } from  'react-native'
 import { AppTextBold } from './ui/AppTextBold'
+import { THEME } from '../theme'
 
 export const Navbar = props => {
     return (
-        <View style={ styles.navbar }>
+        <View style={ { 
+            ...styles.navbar, 
+            ...Platform.select({
+                ios: styles.navBarIOS,
+                android: styles.navbarAndroid
+            })
+        } }>
             <AppTextBold style={ styles.text }>{ props.title }</AppTextBold>
         </View> 
     )
@@ -15,11 +22,17 @@ const styles = StyleSheet.create({
         height: 70,
         alignItems: 'center',
         justifyContent: 'flex-end',
-        backgroundColor: 'firebrick',
         paddingBottom: 10,
+    },
+    navbarAndroid: {
+        backgroundColor: THEME.MAIN_COLOR,
+    },
+    navBarIOS: {
+        backgroundColor: THEME.MAIN_COLOR,
+        borderBottomWidth: 1
     },
     text: {
         fontSize: 26,
-        color: '#f3f3f3',
+        color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : THEME.WHITE_COLOR,
     }
 })
